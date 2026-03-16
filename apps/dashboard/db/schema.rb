e17a10206ab2e041c1aa8141e5a7c2bddde5b195
UltiRequiem <eliaz.bobadilladev@gmail.com>
2026-03-16 10:01:52 -0500

    tweak: fix

--- apps/dashboard/db/schema.rb
+++ apps/dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_10_221218) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_16_145916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_221218) do
     t.string "ip_address"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.index ["created_at"], name: "index_audit_logs_on_created_at"
     t.index ["user_id"], name: "index_audit_logs_on_user_id"
   end
 
